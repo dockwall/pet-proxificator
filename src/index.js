f2039aa3aca33c8b54ef1d53d60f6833edd56160
Stanislav Leshuk <leshuksu@gmail.com>
2023-09-25 16:14:31 +0300

    used cors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors)
+app.use(cors())
 
 require('dotenv').config();
 const PORT = process.env.SERVER_PORT;
 
 app.get('/api/unsplash/search-photos', (req, res) => {
+    res.json({ success: true })
 })
 
 app.listen(PORT, () => console.log(`using port ${PORT}`))
