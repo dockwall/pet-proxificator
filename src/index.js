7cc8ca5bd2e0b4ec510051805d5554747ee7ecdd
Stanislav Leshuk <leshuksu@gmail.com>
2023-09-25 22:47:20 +0300

    created basic limiter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
+
+// Limit user requests
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 3,
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// API routes
 app.use('/api', require('./routes'))
 
 const PORT = process.env.SERVER_PORT;
